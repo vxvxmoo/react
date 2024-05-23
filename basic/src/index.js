226403a312f0bc36bb9f2a6967aff5c3f0d2f4fe
mikan <joooong1021@gmail.com>
2024-05-23 16:13:41 +0900

    add: react 4.16 useState

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppJSX from "./AppJSX";
 import reportWebVitals from "./reportWebVitals";
-import AppProfile from "./AppProfile";
+import AppCounter from "./AppCounter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProfile></AppProfile>
+    <AppCounter />
   </React.StrictMode>
 );
 
