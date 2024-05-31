b7c9913150bb70aeb310b9306a6e739c0e4116b1
mikan <joooong1021@gmail.com>
2024-05-31 13:16:55 +0900

    refactor: react 5.2 마우스 따라가기_by 엘리2(개선)

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -2,19 +2,17 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   return (
     <div
       className="container"
       onPointerMove={(e) => {
-        setX(e.clientX);
-        setY(e.clientY);
+        setPosition({ x: e.clientX, y: e.clientY });
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       />
     </div>
   );
