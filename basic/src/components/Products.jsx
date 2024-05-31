1929d915b3256e3123506a25c73707c1a11cc829
mikan <joooong1021@gmail.com>
2024-05-31 10:44:20 +0900

    add: react 4.20 useEffect 응용

--- basic/src/components/Products.jsx
+++ basic/src/components/Products.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
-    fetch("data/products.json")
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
-        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
+        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴.");
         setProducts(data);
       });
     return () => {
       console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      ></input>
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -28,7 +36,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
