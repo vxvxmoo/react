542a453c9e7848fb7a0f1b4f85796931c07e8676
mikan <joooong1021@gmail.com>
2024-06-06 15:10:39 +0900

    refactor: react 5.12 reducer 사용해보기

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -1,40 +1,26 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+import personReducer from "./reducer/person-reducer";
 
 export default function AppMentor() {
-  const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+  // personReducer: 객체를 새롭게 만들어나갈 로직을 작성해 놓은 함수
+  // initialPerson: 초기값
+  // person: 상태에 접근 가능
+  // dispatch: personReducer를 호출할 수 있음 -> 이를 이용해 원하는 액션을 호출할 수 있음.
+
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        if (mentor.name == prev) {
-          return { ...mentor, name: current };
-        }
-        return mentor;
-      }),
-    }));
+    dispatch({ type: "updated", prev, current });
   };
   const handleAdd = () => {
     const name = prompt(`새로운 멘토의 이름은 무엇인가요?`);
     const title = prompt(`새로운 멘토의 직급은 무엇인가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: [
-        ...person.mentors,
-        {
-          name,
-          title,
-        },
-      ],
-    }));
+    dispatch({ type: "added", name, title });
   };
   const handleDelete = () => {
     const name = prompt(`삭제하려는 멘토의 이름은 무엇인가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.filter((m) => m.name !== name),
-    }));
+    dispatch({ type: "deleted", name });
   };
 
   return (
