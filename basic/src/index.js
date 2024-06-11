59ef8de7dc99d34929f00b72aaae0d2858d95258
mikan <joooong1021@gmail.com>
2024-06-11 11:07:57 +0900

    feat: react 5.14 form을 만드는 법

--- basic/src/index.js
+++ basic/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppMentorsImmer_dyung from "./AppMentorsImmer_dyung";
+import AppForm from "./AppForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentorsImmer_dyung />
+    <AppForm />
   </React.StrictMode>
 );
 
