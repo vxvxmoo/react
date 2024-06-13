0d89f51b4b5619271f6656c17d70e277fdaa1c0e
mikan <joooong1021@gmail.com>
2024-06-13 12:05:18 +0900

    feat: react 5.20 성능 개선 해보기

--- basic/src/AppMentorsButton.jsx
+++ basic/src/AppMentorsButton.jsx
@@ -1,25 +1,25 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useMemo, useCallback, memo } from "react";
 import personReducer from "./reducer/person-reducer";
 
 export default function AppMentorsButton() {
   const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
     dispatch({ type: "updated", prev, current });
-  };
+  }, []);
 
-  const handleAdd = () => {
+  const handleAdd = useCallback(() => {
     const name = prompt(`멘토의 이름은?`);
     const title = prompt(`멘토의 직함은?`);
     dispatch({ type: "added", name, title });
-  };
+  }, []);
 
-  const handleDelete = () => {
+  const handleDelete = useCallback(() => {
     const name = prompt(`누구를 삭제하고 싶은가요?`);
     dispatch({ type: "deleted", name });
-  };
+  }, []);
 
   return (
     <div>
@@ -41,8 +41,9 @@ export default function AppMentorsButton() {
   );
 }
 
-function Button({ text, onClick }) {
+const Button = memo(({ text, onClick }) => {
   console.log("Button", text, "re-rendering 😝");
+  const result = useMemo(() => calculateSomething(), []);
   return (
     <button
       onClick={onClick}
@@ -53,9 +54,16 @@ function Button({ text, onClick }) {
         margin: "0.4rem",
       }}
     >
-      {text}
+      {`${text} ${result}`}
     </button>
   );
+});
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log("꺅!");
+  }
+  return 10;
 }
 
 const initialPerson = {
