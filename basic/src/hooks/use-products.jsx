6ac5c81df35e68c833b0f9aae666dfbb73e5680e
mikan <joooong1021@gmail.com>
2024-06-14 11:35:00 +0900

    feat: react 5.23 커스텀 훅 만들기 🤓

---
+++ basic/src/hooks/use-products.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+export default function useProducts({ salesOnly }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? "sale_" : ""}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴.");
+        setProducts(data);
+      })
+      .catch((error) => {
+        setError("에러가 발생했음");
+      })
+      .finally(() => setLoading(false));
+    return () => {
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
+    };
+  }, [salesOnly]);
+
+  return [loading, error, products];
+}
