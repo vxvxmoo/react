c8f764036d23ce9fa9a83417d9aab789ae6efe40
mikan <joooong1021@gmail.com>
2024-06-18 16:30:32 +0900

    feat: react 6.4 Styled Components

--- css-all/src/App.js
+++ css-all/src/App.js
@@ -1,13 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1rem;
+  padding: 0.25rem 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `};
+`;
 
 function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <Container>
+        <Button>normal</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
     </>
   );
 }
