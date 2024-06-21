a1368756e86b3ed7441e2da98c37a7a401c6a6ec
mikan <joooong1021@gmail.com>
2024-06-21 11:32:41 +0900

    project: react 8.5 추가하기 구현하기 ❓

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -13,6 +13,21 @@ export default function TodoList() {
       status: "active",
     },
   ]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const todoText = e.target.todoText.value;
+
+    if (todoText.trim()) {
+      const newTodo = { id: "125", text: todoText, status: "active" };
+      setTodos([...todos, newTodo]);
+    }
+  };
+
+  const onReset = () => {
+    // input을 어떻게 초기화 해야될지를 모르겠음 ㅠㅠ
+  };
+
   return (
     <section>
       <ul>
@@ -20,6 +35,15 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          name="todoText"
+          placeholder="Add Todo"
+          value={todos.text}
+        ></input>
+        <button type="submit">Add</button>
+      </form>
     </section>
   );
 }
