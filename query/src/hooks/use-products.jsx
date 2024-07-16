efcf09736070c315698e7ab002c4d72337c54ab4
mikan <joooong1021@gmail.com>
2024-07-16 14:56:59 +0900

    project: react 10.4 프로젝트에 적용

--- query/src/hooks/use-products.jsx
+++ query/src/hooks/use-products.jsx
@@ -9,14 +9,14 @@ export default function useProducts({ salesOnly }) {
     console.log("fetching...");
     setLoading(true);
     setError(undefined);
-    fetch(`data/${salesOnly ? "sale_" : ""}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
-        setProducts(data);
-      })
-      .catch((e) => setError("에러가 발생했음! 💀"))
-      .finally(() => setLoading(false));
+    // fetch(`data/${salesOnly ? "sale_" : ""}products.json`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
+    //     setProducts(data);
+    //   })
+    //   .catch((e) => setError("에러가 발생했음! 💀"))
+    //   .finally(() => setLoading(false));
 
     return () => {
       console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
